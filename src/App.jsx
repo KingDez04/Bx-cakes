1f6b0bdd275f17c77c24547f86fc9343c7691880
Kingdez04 <desmondademeso@gmail.com>
2025-11-14 15:58:14 +0100

    chore: setup react-router and toast notifications in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Toaster
+        position="top-center"
+        gutter={16}
+        toastOptions={{
+          duration: 5000,
+          success: { duration: 5000 },
+          error: { duration: 6000 },
+          loading: { duration: Infinity },
+        }}
+      />
+      <Routes>
+        <Route path="*" element={<div>Welcome to the App!</div>} />
+      </Routes>
+    </Router>
+  );
 };
 export default App;
